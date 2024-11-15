4f3046371b6496a22d7a95a599f785f97543d01e
Asylbek Zhunusov <118039268+iamthefamous@users.noreply.github.com>
2024-11-15 11:09:02 +0600

    main file edition + house_analyzer class created

---
+++ src/core/house_analyzer.py
@@ -0,0 +1,24 @@
+class HouseAnalyzer:
+    
+    def apartmentDetails():
+        """
+        Asks userinput for details as an options for each detail
+        and returns the precise details for the given apartment
+        """
+        
+    def privateHouseDetails():
+        """
+        Asks userinput for details as an options for each detail
+        and returns the precise details for the given apartment
+        """
+        
+    
+    def run():
+        print("1. Apartments")
+        print("2. Private House")
+        
+        option = input("Choose you option: ")
+        if option == "1":
+            apartmentDetails()
+        elif option == "2" :
+            privateHouseDetalail()
